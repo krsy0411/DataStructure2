e7b8bf8f399ae77a56a4ee5d3a55c4c0304a31df
PANdas <krsy0411@naver.com>
2023-10-16 17:58:02 +0900

    최대 힙 과제 구현 마무리

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# 링크 표현법과 배열표현법 - 9월 12일
-
-![result](./binaryTree/img/linktree.png);
-
-# 스택과 링크표현법 - 9월 14일
-
-![result](./binaryTree/img/stacktree.png);
-
-# 수식트리 - 9월 19일
-![result](./binaryTree/img/expressionTree.png);
-
-# Traversal2 - 9월 21일
-![result](./binaryTree/img/traversal2.png);
+# 최대 힙 - 10월 16일
+![](./maxHeap.png);
--- maxHeap.c
+++ maxHeap.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_ELEMENT 200
-// boolean 정의
-#define true 1
-#define false 0
 
 // 키값을 지는 원소 구조체
 typedef struct {
     int key;
 } element;
+
 // 크기가 200이고, 히프의 원소 개수를 나타내는 heap_size 선언
 typedef struct {
     element heap[MAX_ELEMENT];
@@ -38,21 +36,59 @@ void insert_max_heap(HeapType* heap, element item)
         heap->heap[i] = heap->heap[i/2];
         i /= 2;
         cnt++;
+        
     }
     // 다 비교하고 나서 : 새 노드를 실제로 삽입
     heap->heap[i] = item;
-    // 삽입된 후의 히프 내부 원소들 전체 출력
-    for(int j=0; j< sizeof(heap)/sizeof(int); j++)
-    {
-        printf('%d ', heap[j]);
-    }
-    printf("\n");
-    printf("%d", cnt);
 }
 
-void delete_max_heap(HeapType* heap, element item)
+element delete_max_heap(HeapType* heap, element item)
 {
+    // 제거할 원소와 인덱스들 변수 선언
+    element temp = heap->heap[(heap->heap_size)--];
+    int delete_item_index, child_index;
 
+    // 우선 제거할 원소의 인덱스를 찾기
+    for(int i=1; i<=heap->heap_size; i++)
+    {
+        if(heap->heap[i].key == item.key)
+        {
+            // 만약 제거할 원소 찾으면 인덱스 저장
+            delete_item_index = i;
+            child_index = i*2;
+        }
+    }
+    // 값 설정
+    while(child_index <= heap->heap_size)
+    {
+        if((child_index < heap->heap_size) && (heap->heap[child_index].key < heap->heap[child_index+1].key))
+        {
+            child_index++;
+        }
+        if(temp.key >= heap->heap[child_index].key)
+        {
+            break;
+        }
+        heap->heap[delete_item_index] = heap->heap[child_index];
+        delete_item_index = child_index;
+        child_index *= 2;
+    }
+    heap->heap[delete_item_index] = temp;
+    return item;
+}
+
+void print_max_heap(HeapType* heap)
+{
+    // 레벨별로 출력
+    for(int i=1; i<=heap->heap_size; i++)
+    {
+        printf("%d ", heap->heap[i].key);
+        if(i==1 || i==3 || i==7)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n");
 }
 
 int main(void)
@@ -82,37 +118,51 @@ int main(void)
     printf("| c: 종료        |\n");
     printf("--------------------\n");
 
-    while(true) {
+    while(1) {
         // 입력받은 메뉴 저장해놓을 변수 선언
         char menu;
         // 입력받은 추가하거나 삭제할 값 저장해놓을 변수 선언
-        int value;  
+        int value;
+        element E;  
         
         // 메뉴 알파벳 입력받기
         printf("메뉴 입력: ");
-        scanf("%c", &menu);
+        scanf(" %c", &menu);
         printf("\n");
 
         switch(menu){
             case 'i':
                 printf("추가할 값 입력: ");
                 scanf("%d", &value);
-                element E = { value };
+                E.key = value;
                 printf("\n");
                 // 삽입 함수 실행
                 insert_max_heap(heap, E);
+                // 삽입된 후의 히프 내부 원소들 전체 출력
+                for(int k=1;k<=heap->heap_size; k++)
+                {
+                    printf("%d ",heap->heap[k].key);
+                }
+                printf("\n");
                 break;
             case 'd':
                 printf("삭제할 값 입력: ");
                 scanf("%d", &value);
-                element E = { value };
+                E.key = value;
                 printf("\n");
                 // 삭제 함수 실행
                 delete_max_heap(heap, E);
+                // 삭제된 후의 히프 내부 원소들 전체 출력
+                for(int k=1;k<=heap->heap_size; k++)
+                {
+                    printf("%d ",heap->heap[k].key);
+                }
+                printf("\n");
                 break;
             case 'p':
                 printf("트리 레벨별 출력");
                 printf("\n");
+                print_max_heap(heap);
                 break;
             default:
                 break;
@@ -122,7 +172,7 @@ int main(void)
             break;
         }
     }
-
-    
+    // 동적할당된 메모리 사용 해제
+    free(heap);
     return 0;
 }
